d86e824c5a880a88b42f9812559e17217ff0af31
Linh Do <hoailinh9966@gmail.com>
2025-05-18 21:35:22 +0700

    remove deprecated firefox option

--- src/main/java/com/senelium/factories/capabilities/FirefoxCapsFactory.java
+++ src/main/java/com/senelium/factories/capabilities/FirefoxCapsFactory.java
@@ -7,8 +7,7 @@ public class FirefoxCapsFactory implements CapabilitiesFactory<FirefoxOptions> {
     @Override
     public FirefoxOptions createCapabilities() {
         var options = new FirefoxOptions();
-        options.setCapability("gpu", false);
-
+        options.addArguments("--disable-gpu");
         options.addPreference("browser.download.dir", FileUtils.getDownloadDir());
         options.addPreference("browser.download.folderList", 2); // 0 = desktop, 1 = default download, 2 = custom
         options.setEnableDownloads(true); // For files download in Grid
